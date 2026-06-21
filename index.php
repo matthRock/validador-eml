f1b33207b48e6979c7d39ce07211b1de1693a4cc
Matheus Rocha <rocham9@gmail.com>
2026-06-21 20:13:52 -0300

    EML-6 #done Cabeçalho do EML separada em linhas

--- index.php
+++ index.php
@@ -35,13 +35,17 @@ $eml_normalizado = str_replace("\r\n", "\n", $eml_bruto);
 # Cortando o EML em cabeçalho e corpo
 $partes_do_eml = explode("\n\n", $eml_normalizado, 2);
 
+# Cortando o cabeçalho do EML para dentro do array
+$partes_do_cabecalho = explode("\n", $partes_do_eml[0]);
 
 $resultado_requisicao = [
     "status" => "200",
     "mensagem" => "Motor PHP e Apache operantes!",
-    "cabecalho_eml" => $partes_do_eml[0],
+    "cabecalho_eml" => $partes_do_cabecalho,
     "corpo_eml" => $partes_do_eml[1] ?? "" # retorna vazio caso o corpo do EML não exista
 ];
+
+
 
 # Exibindo o resultado bem sucedido da requisição
 # e informando o status code 200 (OK) para o cliente
