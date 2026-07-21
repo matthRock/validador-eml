96c680ff7f50b584db31a642fe781d3e008292aa
Matheus Rocha <rocham9@gmail.com>
2026-07-21 08:53:44 -0300

    Refatoração do frontend v1.0 - ajuste de endereço de API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from 'react'
 import './App.css'
+import Editor from '@monaco-editor/react'
 
+// Toda função no react é um componente,
+// Essas funções devem começar com letra maiúscula
+// Para que o react consiga diferenciar componetes de funções normais
 function App() {
 
+  // Guarda mensagem de erro
+  const [mensagemErro, setMensagemErro] = useState('');
+  // Guarda o resultado da análise
+  const [resultadoAnalise, setResultadoAnalise] = useState('');
+
+  const url = import.meta.env.VITE_API_URL;
+
+  async function validaUploadArquivo(event) {
+
+    // Carregando o primeiro arquivo via upload
+    //const arquivo = event.target.files[0];
+
+    // Criando um objeto leitor de arquivos
+    const reader = new FileReader();
+    // var conteudoFinal = new object();
+    //
+
+    reader.onload = async (eventoDeCarregamento) => {
+      const conteudoDoArquivo = eventoDeCarregamento.target.result;
+
+
+      //const conteudoFinal.eml_content = conteudoDoArquivo;
+      try {
+        const resposta = await fetch(url, {
+          method: 'POST',
+          headers:
+          {
+            'Content-Type': 'application/json',
+            // authentication: 'Bearer ' + token;
+          },
+          body: JSON.stringify({ eml_content: conteudoDoArquivo }),
+        });
+        setResultadoAnalise(await resposta.json());
+      } catch (erro) {
+        setMensagemErro('Erro ao processar a resposta do servidor: ' + erro.message);
+      }
+    };
+    reader.readAsText(event.target.files[0]);
+  }
 
   return (
     <>
@@ -10,11 +54,11 @@ function App() {
       </header>
       <section>
         <div className="editor">
-          <input type="file" accept=".eml" className="arquivo_eml"/>
+          <input type="file" accept=".eml" onChange={validaUploadArquivo} />
+          {mensagemErro && <p className="error">{mensagemErro}</p>}
         </div>
-        <iframe>
-          <p>Aqui será exido o restulado da requisição</p>
-        </iframe>
+        {resultadoAnalise &&
+          <code>{JSON.stringify(resultadoAnalise, null, 2)}</code>}
       </section>
       <footer>
         <p>@2026 All rights reserved</p>
@@ -22,6 +66,6 @@ function App() {
       </footer>
     </>
   )
-
 }
+
 export default App
